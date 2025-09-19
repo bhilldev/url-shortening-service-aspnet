a5613525de79de238333712b29a4e4a82ea39ca2
Brandon Hill <bhilldev@outlook.com>
2025-09-19 15:00:56 -0500

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UrlShorteningService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
